dcf7a31f77e1cbeda195ece91e1c38eca497dd5c
Chiamaka Uyanna <chiamakauyanna@gmail.com>
2025-03-30 12:40:37 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,9 @@
-import { useEffect } from "react";
-import About from "./components/layout/About";
-import Contact from "./components/layout/Contact";
-import Footer from "./components/layout/Footer";
-import Header from "./components/layout/Header";
-import Hero from "./components/layout/Hero";
-import Portfolio from "./components/layout/Portfolio";
-import Skills from "./components/layout/Skills";
-import AOS from "aos";
-import "aos/dist/aos.css";
-
 const App = () => {
-  useEffect(() => {
-    AOS.init({
-      offset: 300,
-      duration: 1400,
-      once: true,
-      mirror: false,
-    });
-  }, []);
-  
   return (
-    <div className="font-ubuntu overflow-x-hidden">
-      <Header />
-      <Hero />
-      <About />
-      <Skills />
-      <Portfolio />
-      <Contact />
-      <Footer />
+    <div>
+      <h1>Hello World</h1>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
--- src/components/common/Logo.jsx
+++
@@ -1,9 +0,0 @@
-const Logo = () => {
-  return (
-    <div className='flex gap-2 text-2xl font-bold rounded-full border-4 border-accent px-4 py-2'>
-      <h3 className='text-accent font-mono'>C</h3>
-    </div>
-  )
-}
-
-export default Logo
